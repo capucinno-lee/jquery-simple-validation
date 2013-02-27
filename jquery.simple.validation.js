6f56273e02e223e51e38e7e87afe71640ee5b322
capucinno.lee@gmail.com <capucinno.lee@gmail.com>
2013-02-27 10:32:54 +0700

    update: checking visibility

--- jquery.simple.validation.js
+++ jquery.simple.validation.js
@@ -65,151 +65,154 @@
 		return $(form.selector).submit(function(ev) {
 			var warning = '';
 			$(form.selector).find('input, select, textarea').each(function() {
-				var data_rules = $(this).data('rules');
-				if( data_rules != '' && data_rules != undefined ) {
-					var validation = data_rules.split('#');
-					for( var x=0; x<validation.length; x++ ) {
-						var rule = validation[x].split('-');
-							var label = rule[0];
-							var type = rule[1];
-							var value = $(this).val();
 
-						if( type == 'required' && value == '' ) {
-							warning += 'The ' + label + ' field is required.\n';
-						}
+				if( $(this).is(':visible') ) {
+					var data_rules = $(this).data('rules');
+					if( data_rules != '' && data_rules != undefined ) {
+						var validation = data_rules.split('#');
+						for( var x=0; x<validation.length; x++ ) {
+							var rule = validation[x].split('-');
+								var label = rule[0];
+								var type = rule[1];
+								var value = $(this).val();
 
-						else if( type == 'valid_email' && !methods.isEmail(value) ) {
-							warning += 'The ' + label + ' field must contain a valid email address.\n';
-						}
-
-						else if( type == 'valid_url' && value != '' && !methods.isUrl(value) ) {
-							warning += 'The ' + label + ' field must contain a valid URL.\n';
-						}
-
-						else if( type == 'valid_ip' && value != '' && !methods.isIp(value) ) {
-							warning += 'The ' + label + ' field must contain a valid IP.\n';
-						}
-
-						else if( type.substr(0, 10) == 'min_length' && value != '' ) {
-							temp = type.split('+');
-							length = temp[1];
-							if( value.length < length ) {
-								suffix = '';
-								if( length > 1 )
-									suffix = 's';
-								warning += 'The ' + label + ' field must be at least ' + length + ' character' + suffix + ' in length.\n';
+							if( type == 'required' && value == '' ) {
+								warning += 'The ' + label + ' field is required.\n';
 							}
-						}
 
-						else if( type.substr(0, 10) == 'max_length' && value != '' ) {
-							temp = type.split('+');
-							length = temp[1];
-							if( value.length > length ) {
-								suffix = '';
-								if( length > 1 )
-									suffix = 's';
-								warning += 'The ' + label + ' field can not exceed ' + length + ' character' + suffix + ' in length.\n';
+							else if( type == 'valid_email' && !methods.isEmail(value) ) {
+								warning += 'The ' + label + ' field must contain a valid email address.\n';
 							}
-						}
 
-						else if( type.substr(0, 12) == 'exact_length' && value != '' ) {
-							temp = type.split('+');
-							length = temp[1];
-							if( value.length != length ) {
-								suffix = '';
-								if( length > 1 )
-									suffix = 's';
-								warning += 'The ' + label + ' field must be exactly ' + length + ' character' + suffix + ' in length.\n';
+							else if( type == 'valid_url' && value != '' && !methods.isUrl(value) ) {
+								warning += 'The ' + label + ' field must contain a valid URL.\n';
 							}
-						}
 
-						else if( type.substr(0, 3) == 'min' && value != '' && methods.isInteger(value) ) {
-							temp = type.split('+');
-							limit = temp[1];
-							if( value < limit ) {
-								warning += 'The ' + label + ' field must be greater than ' + limit + '.\n';
+							else if( type == 'valid_ip' && value != '' && !methods.isIp(value) ) {
+								warning += 'The ' + label + ' field must contain a valid IP.\n';
 							}
-						}
 
-						else if( type.substr(0, 3) == 'max' && value != '' && methods.isInteger(value) ) {
-							temp = type.split('+');
-							limit = temp[1];
-							if( value > limit ) {
-								warning += 'The ' + label + ' field must be less than ' + limit + '.\n';
-							}
-						}
-
-						else if( type == 'alpha' && !methods.isAlpha(value) ) {
-							warning += 'The ' + label + ' field may only contain alphabetical characters.\n';
-						}
-
-						else if( type == 'alpha_numeric' && !methods.isAlphaNum(value) ) {
-							warning += 'The ' + label + ' field may only contain alpha-numeric characters.\n';
-						}
-
-						else if( type == 'numeric' && value != '' && parseFloat(value).toString() != value ) {
-							warning += 'The ' + label + ' field must contain only numbers.\n';
-						}
-
-						else if( type == 'is_numeric' && !methods.isNumeric(value) ) {
-							warning += 'The ' + label + ' field must contain only numeric characters.\n';
-						}
-
-						else if( type == 'integer' && value != '' && !methods.isInteger(value) ) {
-							warning += 'The ' + label + ' field must contain an integer.\n';
-						}
-
-						else if( type.substr(0, 7) == 'matches' && value != '' ) {
-							temp = type.split('+');
-							match_id = temp[1];
-							match_label = temp[2];
-							match_value = $('#'+match_id).val();
-							if( value != match_value ) {
-								warning += 'The ' + label + ' field does not match the ' + match_label + ' field.\n';
-							}
-						}
-
-						else if( type == 'valid_phone' && value != '' && !methods.isPhone(value) ) {
-							warning += 'The ' + label + ' field must contain a valid phone number.\n';
-						}
-
-						else if( type == 'valid_date' && value != '' && !methods.isDate(value) ) {
-							warning += 'The ' + label + ' field must contain a valid date.\n';
-						}
-
-						else if( type.substr(0, 11) == 'valid_check' ) {
-							if( $(this).attr('checked') != 'checked' )
-								warning += label + '\n';
-						}
-
-						else if( type.substr(0, 11) == 'valid_radio' ) {
-							match_class = $(this).attr('name');
-							flag = 0;
-							$(form.selector).find('input:radio').each(function() {
-								if( $(this).attr('name') == match_class  && $(this).attr('checked') == 'checked' ) {
-									flag++;
+							else if( type.substr(0, 10) == 'min_length' && value != '' ) {
+								temp = type.split('+');
+								length = temp[1];
+								if( value.length < length ) {
+									suffix = '';
+									if( length > 1 )
+										suffix = 's';
+									warning += 'The ' + label + ' field must be at least ' + length + ' character' + suffix + ' in length.\n';
 								}
-							});
-							if( flag == 0 )
-								warning += 'Please choose one of ' + label + ' options.\n';
-						}
+							}
 
-						else if( type.substr(0, 11) == 'multi_check' ) {
-							temp = type.split('+');
-							match_unit = temp[1];
-							match_class = $(this).attr('name');
-							flag = 0;
-							$(form.selector).find('input:checkbox').each(function() {
-								if( $(this).attr('name') == match_class  && $(this).attr('checked') == 'checked' ) {
-									flag++;
+							else if( type.substr(0, 10) == 'max_length' && value != '' ) {
+								temp = type.split('+');
+								length = temp[1];
+								if( value.length > length ) {
+									suffix = '';
+									if( length > 1 )
+										suffix = 's';
+									warning += 'The ' + label + ' field can not exceed ' + length + ' character' + suffix + ' in length.\n';
 								}
-							});
-							if( flag < match_unit )
-								warning += 'Please choose (min) ' + match_unit + ' of ' + label + ' options.\n';
-						}
+							}
 
-					} //end for
-				}
+							else if( type.substr(0, 12) == 'exact_length' && value != '' ) {
+								temp = type.split('+');
+								length = temp[1];
+								if( value.length != length ) {
+									suffix = '';
+									if( length > 1 )
+										suffix = 's';
+									warning += 'The ' + label + ' field must be exactly ' + length + ' character' + suffix + ' in length.\n';
+								}
+							}
+
+							else if( type.substr(0, 3) == 'min' && value != '' && methods.isInteger(value) ) {
+								temp = type.split('+');
+								limit = temp[1];
+								if( value < limit ) {
+									warning += 'The ' + label + ' field must be greater than ' + limit + '.\n';
+								}
+							}
+
+							else if( type.substr(0, 3) == 'max' && value != '' && methods.isInteger(value) ) {
+								temp = type.split('+');
+								limit = temp[1];
+								if( value > limit ) {
+									warning += 'The ' + label + ' field must be less than ' + limit + '.\n';
+								}
+							}
+
+							else if( type == 'alpha' && !methods.isAlpha(value) ) {
+								warning += 'The ' + label + ' field may only contain alphabetical characters.\n';
+							}
+
+							else if( type == 'alpha_numeric' && !methods.isAlphaNum(value) ) {
+								warning += 'The ' + label + ' field may only contain alpha-numeric characters.\n';
+							}
+
+							else if( type == 'numeric' && value != '' && parseFloat(value).toString() != value ) {
+								warning += 'The ' + label + ' field must contain only numbers.\n';
+							}
+
+							else if( type == 'is_numeric' && !methods.isNumeric(value) ) {
+								warning += 'The ' + label + ' field must contain only numeric characters.\n';
+							}
+
+							else if( type == 'integer' && value != '' && !methods.isInteger(value) ) {
+								warning += 'The ' + label + ' field must contain an integer.\n';
+							}
+
+							else if( type.substr(0, 7) == 'matches' && value != '' ) {
+								temp = type.split('+');
+								match_id = temp[1];
+								match_label = temp[2];
+								match_value = $('#'+match_id).val();
+								if( value != match_value ) {
+									warning += 'The ' + label + ' field does not match the ' + match_label + ' field.\n';
+								}
+							}
+
+							else if( type == 'valid_phone' && value != '' && !methods.isPhone(value) ) {
+								warning += 'The ' + label + ' field must contain a valid phone number.\n';
+							}
+
+							else if( type == 'valid_date' && value != '' && !methods.isDate(value) ) {
+								warning += 'The ' + label + ' field must contain a valid date.\n';
+							}
+
+							else if( type.substr(0, 11) == 'valid_check' ) {
+								if( $(this).attr('checked') != 'checked' )
+									warning += label + '\n';
+							}
+
+							else if( type.substr(0, 11) == 'valid_radio' ) {
+								match_class = $(this).attr('name');
+								flag = 0;
+								$(form.selector).find('input:radio').each(function() {
+									if( $(this).attr('name') == match_class  && $(this).attr('checked') == 'checked' ) {
+										flag++;
+									}
+								});
+								if( flag == 0 )
+									warning += 'Please choose one of ' + label + ' options.\n';
+							}
+
+							else if( type.substr(0, 11) == 'multi_check' ) {
+								temp = type.split('+');
+								match_unit = temp[1];
+								match_class = $(this).attr('name');
+								flag = 0;
+								$(form.selector).find('input:checkbox').each(function() {
+									if( $(this).attr('name') == match_class  && $(this).attr('checked') == 'checked' ) {
+										flag++;
+									}
+								});
+								if( flag < match_unit )
+									warning += 'Please choose (min) ' + match_unit + ' of ' + label + ' options.\n';
+							}
+
+						} //end for
+					} //if set data-rules
+				} //if visible
 			});
 
 			if( warning != '' ) {
